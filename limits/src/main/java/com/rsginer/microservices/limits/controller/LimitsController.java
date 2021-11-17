14c7da1a0b7bf41362ff467bb2e8c8a4b611614e
Ruben Soler <ruben.soler@mimacom.com>
2021-11-17 18:37:37 +0100

    Init

---
+++ limits/src/main/java/com/rsginer/microservices/limits/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.rsginer.microservices.limits.controller;
+
+import com.rsginer.microservices.limits.bean.Limits;
+import com.rsginer.microservices.limits.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+}
---
+++ limits/src/test/java/com/rsginer/microservices/limits/LimitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsginer.microservices.limits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
